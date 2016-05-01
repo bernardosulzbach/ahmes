4bfd8f78a24a24e9458abd7b38dde74a5a044fe0
Bernardo Sulzbach <mafagafogigante@gmail.com>
2016-05-01 02:57:48 -0300

    Started working on the AhmesComputer class

--- ahmes.py
+++ ahmes.py
@@ -1,10 +1,78 @@
 #!/usr/bin/python
 
 
+def find_maximum_string_width(objects):
+    maximum_so_far = 0
+    for i, e in enumerate(objects):
+        maximum_so_far = max(maximum_so_far, len(str(e)))
+    return maximum_so_far
+
+
+def make_list_of_key_value_lines(keys, values):
+    assert len(keys) == len(values), 'keys and values should have the same length'
+    lines = []
+    maximum_key_width = find_maximum_string_width(keys)
+    maximum_value_width = find_maximum_string_width(values)
+    # Doubling the '{' and the '}' prevents the formatter from parsing them.
+    format_string = '{{0:{0}}}: {{1:{1}}}'.format(maximum_key_width, maximum_value_width)
+    for i, e in enumerate(keys):
+        lines.append(format_string.format(e, values[i]))
+    return lines
+
+
+def make_string_of_key_value_lines(keys, values):
+    return '\n'.join(make_list_of_key_value_lines(keys, values))
+
+
+def is_byte(integer):
+    """
+    Returns whether or not an integer value could be stored in a byte of 8 bits.
+    :param integer: any int
+    """
+    assert isinstance(integer, int), 'integer should be an int'
+    return 0 <= integer <= 255
+
+
+class AhmesComputer(object):
+    """
+    A pure Python implementation of the Ahmes computer.
+    """
+
+    def __init__(self, ac=0, pc=0):
+        self.ac = 0  # It is a good practice to define all attributes inside the __init__ method
+        self.pc = 0
+        self.set_ac(ac)  # Should reuse the AC setter so that the validation step is not duplicated
+        self.set_pc(pc)  # Same for PC
+        self.bytes = [0] * 256
+
+    def set_ac(self, ac):
+        assert is_byte(ac), 'ac should be a valid byte value'
+        self.ac = ac
+
+    def set_pc(self, pc):
+        assert is_byte(pc), 'pc should be a valid byte value'
+        self.pc = pc
+
+    def load_program(self, program):
+        assert isinstance(program, AhmesProgram), 'program should be an AhmesProgram'
+        self.bytes = list(program.bytes)  # Copy the bytes, not just the reference
+
+    def advance(self):
+        pass
+
+    def __str__(self):
+        keys = ['AC', 'PC']
+        keys.extend([i for i in range(256)])
+        values = [self.ac, self.pc]
+        values.extend(self.bytes)
+        return make_string_of_key_value_lines(keys, values)
+
+
 class AhmesProgram(object):
     """
     A pure Python representation of an Ahmes program.
     """
+
     def __init__(self, filename):
         self.initialized = False
         self.filename = filename
@@ -33,21 +101,22 @@ class AhmesProgram(object):
         :param byte_list: a list of bytes
         """
         if self.initialized:
-            assert len(byte_list) == len(self.get_bytes()), 'byte_list should have the same size as the current list'
+            assert len(byte_list) == len(
+                    self.get_bytes()), 'byte_list should have the same size as the current list'
             assert all(map(lambda e: isinstance(e, int), byte_list)), 'byte_list should be a list of bytes'
             self.bytes = byte_list
 
     def __str__(self):
         if not self.initialized:
-            return "Failed to initialize the program."
-        length_of_largest_index = len(str(len(self.bytes)))
-        lines = []
-        for i, e in enumerate(self.get_bytes()):
-            lines.append(str(i).rjust(length_of_largest_index) + ": " + str(e))
-        return '\n'.join(lines)
+            return 'Failed to initialize the program.'
+        keys = [i for i in range(256)]
+        values = self.bytes
+        return make_string_of_key_value_lines(keys, values)
 
 
 if __name__ == '__main__':
-    program = AhmesProgram('ones.mem')
-    program.set_bytes(program.get_bytes())
-    print(str(program))
+    computer = AhmesComputer(16, 32)
+    print(str(computer))
+    ahmes_program = AhmesProgram('ones.mem')
+    ahmes_program.set_bytes(ahmes_program.get_bytes())
+    print(str(ahmes_program))
